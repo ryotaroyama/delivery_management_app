1bb76599f876afb5925a33529d4e58186a12c6b7
ryotaroyama <ultrared3@yahoo.co.jp>
2021-04-07 10:26:45 +0900

    productsコントローラーの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,33 @@
 class ProductsController < ApplicationController
-  def show
-  end
-
   def new
+    @product = Product.new
   end
 
   def create
+    product = Product.create!(product_params)
+    redirect_to customer
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+    product.update!(product_params)
+    redirect_to customer
   end
 
   def destroy
   end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :process, :delivery_date, :drawing_number, :comment)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "customers#index"
   resources :customers, only: [:index]
+  resources :products, only: [:create, :edit, :show, :destroy]
 end
