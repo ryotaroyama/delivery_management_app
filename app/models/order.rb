5ceb7ea60d0e5aea2077cf9ca0badcdfd3b36eaf
ryotaroyama <ultrared3@yahoo.co.jp>
2021-04-13 22:12:58 +0900

    列挙型の修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,12 @@ class Order < ApplicationRecord
   validates :process, presence: true
   validates :drawing_number, length: { maximum: 20 }
   validates :comment, length: { maximum: 50 }
+
+  enum process: {
+    painting: 1,
+    polishing: 2,
+    processing: 3,
+    alumite: 4,
+    others: 5,
+  }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,4 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :drawing_number, length: { maximum: 20 }
 
-  enum process: {
-    painting: 1,
-    polishing: 2,
-    processing: 3,
-    alumite: 4,
-    others: 5,
-  }
 end
