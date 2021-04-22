aa77fcde53e2857fad40a223cf377fcf9c8575bd
ryotaroyama <ultrared3@yahoo.co.jp>
2021-04-22 09:33:50 +0900

    実装中

--- app/controllers/drawing_numbers_controller.rb
+++ app/controllers/drawing_numbers_controller.rb
@@ -1,6 +1,8 @@
 class DrawingNumbersController < ApplicationController
-  # def search_number
-  #   product = DrawingNumber.find_by(name: params["product_name"])
-  #   render json: { drawing_number: product.drawing_number }
-  # end
+  def search_number
+    r = Product.find_by(name:params[:order][:product_name]).id
+    k = Order.(find_by(product_id:r).id).drawing_number_id
+    number = DrawingNumber.find_by(id:k)
+    render json: { name: number.name}
+  end
 end
