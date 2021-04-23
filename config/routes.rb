46d4c2353519762db42616e375c8f3acf9c87159
ryotaroyama <ultrared3@yahoo.co.jp>
2021-04-23 23:54:36 +0900

    セレクトボックスの初期値が出るように実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "orders#index"
-  resources :orders, only: [:index, :show, :new, :create, :edit, :destroy]
+  resources :orders, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   get "/drawing_numbers/search_number", to: "drawing_numbers#search_number"
 end
