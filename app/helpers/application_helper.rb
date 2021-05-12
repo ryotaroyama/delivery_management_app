02020de280444bc113ec179096db2290d8b80820
ryotaroyama <ultrared3@yahoo.co.jp>
2021-05-12 18:12:19 +0900

    詳細画面の均等割付、他画面の調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def per_capital_rate(str)
+    new_str = ""
+    str.chars.each do |char|
+      new_str << "<span>#{char}</span>"
+    end
+    new_str.html_safe
+  end
 end
