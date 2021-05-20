6ce6bd036440ce58a04fb5b4a194a9177be80bac
ryotaroyama <ultrared3@yahoo.co.jp>
2021-05-20 09:18:56 +0900

    テスト

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :line, ENV["LINE_KEY"], ENV["LINE_SECRET"]
+  provider :line, ENV["LINE_KEY"], ENV["LINE_SECRET"], { provider_ignores_state: true }
 end
