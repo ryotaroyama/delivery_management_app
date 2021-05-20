15c30fcd846878fe16102ef92e32771b1eafac6d
ryotaroyama <ultrared3@yahoo.co.jp>
2021-05-20 15:23:33 +0900

    binding.pry削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -35,7 +35,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
     def basic_action
-      binding.pry
       @omniauth = request.env["omniauth.auth"]
       if @omniauth.present?
         @profile = User.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
