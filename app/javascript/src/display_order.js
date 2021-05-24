07b6f704a876acbbc3490506b33dd73330e7ca94
ryotaroyama <ultrared3@yahoo.co.jp>
2021-05-24 16:29:25 +0900

    JSの読み込みに関するリファクタリング

--- app/javascript/src/display_order.js
+++ app/javascript/src/display_order.js
@@ -1,24 +1,19 @@
 // 表示、非表示だけのクリックイベント
-document.addEventListener('turbolinks:load', () => {
-  const displays = document.querySelectorAll('.display')
+export const removeOrder = (e) => {
+  const orderId = e.target.closest('.order').dataset.id
+  const method = location.href.indexOf('?display=none') == -1 ? 'DELETE' : 'POST'
   const csrfToken = document.querySelector('meta[name=csrf-token]').content
-  displays.forEach((display) => {
-    display.addEventListener('click', (e) => {
-      const orderId = e.target.closest('.order').dataset.id
-      const method = location.href.indexOf('?display=none') == -1 ? 'DELETE' : 'POST'
 
-      fetch(`/orders/${orderId}/display_orders`, {
-        method: method,
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': csrfToken,
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          // 対応する注文をフロント側から消去
-          document.querySelector(`div[data-id="${data.order_id}"]`).remove()
-        })
-    })
+  fetch(`/orders/${orderId}/display_orders`, {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': csrfToken,
+    },
   })
-})
+    .then((response) => response.json())
+    .then((data) => {
+      // 対応する注文をフロント側から消去
+      document.querySelector(`div[data-id="${data.order_id}"]`).remove()
+    })
+}
--- app/javascript/src/order.js
+++ app/javascript/src/order.js
@@ -1,14 +1,9 @@
-document.addEventListener('turbolinks:load', () => {
-  const screens = document.querySelectorAll('.order-name')
-  screens.forEach((screen) => {
-    screen.addEventListener('click', (e) => {
-      const orderProduct = document.getElementById('order-product-name')
-      const orderDeliveryDate = document.getElementById('order-delivery-date')
-      const orderComment = document.getElementById('order-comment')
-      const dataset = e.target.dataset
-      orderProduct.textContent = dataset.order_product_name
-      orderDeliveryDate.textContent = dataset.order_delivery_date
-      orderComment.textContent = dataset.order_comment
-    })
-  })
-})
+export const setDataToModal = (e) => {
+  const orderProduct = document.getElementById('order-product-name')
+  const orderDeliveryDate = document.getElementById('order-delivery-date')
+  const orderComment = document.getElementById('order-comment')
+  const dataset = e.target.dataset
+  orderProduct.textContent = dataset.order_product_name
+  orderDeliveryDate.textContent = dataset.order_delivery_date
+  orderComment.textContent = dataset.order_comment
+}
