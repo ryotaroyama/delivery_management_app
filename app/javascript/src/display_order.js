596d760ad8a07dac2c723fd2c627ca27cb796f63
ryotaroyama <ultrared3@yahoo.co.jp>
2021-05-25 15:13:24 +0900

    fetch時のエラー処理

--- app/javascript/src/display_order.js
+++ app/javascript/src/display_order.js
@@ -1,19 +1,36 @@
 // 表示、非表示だけのクリックイベント
-export const removeOrder = (e) => {
+export const removeOrder = async (e) => {
   const orderId = e.target.closest('.order').dataset.id
   const method = location.href.indexOf('?display=none') == -1 ? 'DELETE' : 'POST'
   const csrfToken = document.querySelector('meta[name=csrf-token]').content
 
-  fetch(`/orders/${orderId}/display_orders`, {
-    method: method,
-    headers: {
-      'Content-Type': 'application/json',
-      'X-CSRF-Token': csrfToken,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // 対応する注文をフロント側から消去
-      document.querySelector(`div[data-id="${data.order_id}"]`).remove()
+  try {
+    const response = await fetch(`/orders/${orderId}/display_orders`, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': csrfToken,
+      },
     })
+    if (!response.ok) throw new Error('処理に失敗しました。リロードしてお試し下さい。')
+
+    const data = await response.json()
+
+    // 対応する注文をフロント側から消去
+    document.querySelector(`div[data-id="${data.order_id}"]`).remove()
+  } catch (error) {
+    if (error.message === 'Failed to fetch') {
+      alert('通信に失敗しました。インターネットの接続状況をご確認下さい。')
+    } else {
+      alert(error.message)
+    }
+  }
+
+  // response
+
+  // .then((response) => response.json())
+  // .then((data) => {
+  //   // 対応する注文をフロント側から消去
+  //   document.querySelector(`div[data-id="${data.order_id}"]`).remove()
+  // })
 }
