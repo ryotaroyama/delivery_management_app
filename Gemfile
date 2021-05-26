618e0e3771409748a5496f03e453821b3c7da261
ryotaroyama <ultrared3@yahoo.co.jp>
2021-05-26 16:11:36 +0900

    RuboCopの対応

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'pry-byebug'
+  gem 'pry-rails'
   gem 'rubocop-performance', require: false
   gem 'rubocop-rails', require: false
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,29 +4,8 @@ class User < ApplicationRecord
   # :registerable, :recoverable, :rememberable, :validatable,
   devise :database_authenticatable, :omniauthable, omniauth_providers: %i[line]
 
+  # Devise のエラーを回避
   attr_accessor :provider
-
-  def social_profile(provider)
-    social_profiles.find { |sp| sp.provider == provider.to_s }
-  end
-
-  def set_values(omniauth)
-    return if provider.to_s != omniauth['provider'].to_s || uid != omniauth['uid']
-
-    # credentials = omniauth['credentials']
-    # info = omniauth['info']
-
-    # access_token = credentials['refresh_token']
-    # access_secret = credentials['secret']
-    # credentials = credentials.to_json
-    # name = info['name']
-    # set_values_by_raw_info(omniauth['extra']['raw_info'])
-  end
-
-  # def set_values_by_raw_info(raw_info)
-  #   self.raw_info = raw_info.to_json
-  #   save!
-  # end
 
   def encrypted_password; end
 end
