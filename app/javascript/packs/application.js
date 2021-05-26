e456ca8b79e90ac31d35761f73e196effb993c40
ryotaroyama <ultrared3@yahoo.co.jp>
2021-05-26 17:16:59 +0900

    flatpickrのcssを移動

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from '@rails/ujs'
 import Turbolinks from 'turbolinks'
 import * as ActiveStorage from '@rails/activestorage'
 import 'channels'
-
+import 'flatpickr/dist/themes/material_blue.css'
 import '../src/jquery.flexselect'
 
 Rails.start()
