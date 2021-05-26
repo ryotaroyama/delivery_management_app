1bb8ae8a95421bcc733007149a935e6703cdf8e9
ryotaroyama <ultrared3@yahoo.co.jp>
2021-05-26 13:38:02 +0900

    RuboCopの自動修正と一部修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,9 +3,9 @@ class OrdersController < ApplicationController
   before_action :set_relations, only: %i[show edit]
 
   def index
-    column = "orders.id, orders.delivery_date, orders.comment, customers.name as customer_name, products.name as product_name"
+    column = 'orders.id, orders.delivery_date, orders.comment, customers.name as customer_name, products.name as product_name'
     # 表示・非表示の設定
-    @display = params[:display] != "none"
+    @display = params[:display] != 'none'
     @orders = Order.where(display: @display).joins(:customer, :product).select(column).order(delivery_date: :asc, created_at: :asc)
   end
 
@@ -17,26 +17,15 @@ class OrdersController < ApplicationController
   end
 
   def create
-    customer = Customer.find_or_create_by!(customer_params)
-    processor = Processor.find_or_create_by!(processor_params)
-    product = Product.find_or_create_by!(product_params)
-    drawing_number = nil
+    @order = Order.new(order_params)
+    set_drawing_number if drawing_number_params[:name].present?
 
-    order = Order.new(order_params)
-    if drawing_number_params[:name].present?
-      drawing_number = DrawingNumber.find_or_create_by!(drawing_number_params)
-      order.drawing_number_id = drawing_number.id
-    end
-
-    order.customer_id = customer.id
-    order.product_id = product.id
-    order.processor_id = processor.id
-    order.save!
+    set_order_params
+    @order.save!
     redirect_to orders_path
   end
 
-  def show
-  end
+  def show; end
 
   def edit
     @customer_names = Customer.pluck(:name)
@@ -45,44 +34,57 @@ class OrdersController < ApplicationController
   end
 
   def update
-    @order.customer_id = Customer.find_or_create_by!(customer_params).id
-    @order.product_id = Product.find_or_create_by!(product_params).id
-    @order.processor_id = Processor.find_or_create_by!(processor_params).id
-    @order.drawing_number_id = DrawingNumber.find_or_create_by!(drawing_number_params).id
+    set_order_params
+    set_drawing_number if drawing_number_params[:name].present?
     @order.update!(order_params)
     redirect_to orders_path
   end
 
   private
 
-    def set_order
-      @order = Order.find(params[:id])
-    end
+  def set_order_params
+    customer = Customer.find_or_create_by!(customer_params)
+    processor = Processor.find_or_create_by!(processor_params)
+    product = Product.find_or_create_by!(product_params)
 
-    def set_relations
-      @customer = @order.customer
-      @product = @order.product
-      @processor = @order.processor
-      @drawing_number = @order.drawing_number
-    end
+    @order.customer_id = customer.id
+    @order.product_id = product.id
+    @order.processor_id = processor.id
+  end
 
-    def customer_params
-      params.require(:customer).permit(:name)
-    end
+  def set_drawing_number
+    drawing_number = DrawingNumber.find_or_create_by!(drawing_number_params)
+    @order.drawing_number_id = drawing_number.id
+  end
 
-    def processor_params
-      params.require(:processor).permit(:name)
-    end
+  def set_order
+    @order = Order.find(params[:id])
+  end
 
-    def product_params
-      params.require(:product).permit(:name)
-    end
+  def set_relations
+    @customer = @order.customer
+    @product = @order.product
+    @processor = @order.processor
+    @drawing_number = @order.drawing_number
+  end
 
-    def drawing_number_params
-      params.require(:drawing_number).permit(:name)
-    end
+  def customer_params
+    params.require(:customer).permit(:name)
+  end
 
-    def order_params
-      params.require(:order).permit(:delivery_date, :process, :comment)
-    end
+  def processor_params
+    params.require(:processor).permit(:name)
+  end
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
+
+  def drawing_number_params
+    params.require(:drawing_number).permit(:name)
+  end
+
+  def order_params
+    params.require(:order).permit(:delivery_date, :process, :comment)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,27 +7,26 @@ class User < ApplicationRecord
   attr_accessor :provider
 
   def social_profile(provider)
-    social_profiles.select {|sp| sp.provider == provider.to_s }.first
+    social_profiles.find { |sp| sp.provider == provider.to_s }
   end
 
   def set_values(omniauth)
-    return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
+    return if provider.to_s != omniauth['provider'].to_s || uid != omniauth['uid']
 
-    credentials = omniauth["credentials"]
-    info = omniauth["info"]
+    # credentials = omniauth['credentials']
+    # info = omniauth['info']
 
-    access_token = credentials["refresh_token"]
-    access_secret = credentials["secret"]
-    credentials = credentials.to_json
-    name = info["name"]
-    # self.set_values_by_raw_info(omniauth['extra']['raw_info'])
+    # access_token = credentials['refresh_token']
+    # access_secret = credentials['secret']
+    # credentials = credentials.to_json
+    # name = info['name']
+    # set_values_by_raw_info(omniauth['extra']['raw_info'])
   end
 
-  def set_values_by_raw_info(raw_info)
-    self.raw_info = raw_info.to_json
-    self.save!
-  end
+  # def set_values_by_raw_info(raw_info)
+  #   self.raw_info = raw_info.to_json
+  #   save!
+  # end
 
-  def encrypted_password
-  end
+  def encrypted_password; end
 end
