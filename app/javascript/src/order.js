514f488a3d7c43feeb55257adf0251348dd88fcf
ryotaroyama <ultrared3@yahoo.co.jp>
2021-05-27 15:53:10 +0900

    CSSの調整

--- app/javascript/src/order.js
+++ app/javascript/src/order.js
@@ -1,8 +1,10 @@
 export const setDataToModal = (e) => {
+  const orderCustomer = document.getElementById('order-customer-name')
   const orderProduct = document.getElementById('order-product-name')
   const orderDeliveryDate = document.getElementById('order-delivery-date')
   const orderComment = document.getElementById('order-comment')
   const dataset = e.target.dataset
+  orderCustomer.textContent = dataset.order_customer_name
   orderProduct.textContent = dataset.order_product_name
   orderDeliveryDate.textContent = dataset.order_delivery_date
   orderComment.textContent = dataset.order_comment
