f8e8661ac097e60135f20c50b9cbfc75e9c0f803
ryotaroyama <ultrared3@yahoo.co.jp>
2021-06-24 16:48:24 +0900

    lineログインしないように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, if: -> { Rails.env.production? && ENV['HEROKU_ENV'] != 'staging' }
 end
 # , if: -> { Rails.env.production? && ENV['HEROKU_ENV'] != 'staging' }
