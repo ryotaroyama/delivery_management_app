64e9699aa96cebe49de96a8e97e4cee417fd174d
ryotaroyama <ultrared3@yahoo.co.jp>
2021-06-29 20:51:36 +0900

    ログインコメント

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, if: -> { Rails.env.production? && ENV['HEROKU_ENV'] != 'staging' }
+  before_action :authenticate_user!
 end
 # , if: -> { Rails.env.production? && ENV['HEROKU_ENV'] != 'staging' }
+# ユーザーがログインしているかどうか確認する！before_action :authenticate_user!
