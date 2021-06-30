9339066524f8b1c7f2ba768a5b98f6801dc8fcb6
ryotaroyama <ultrared3@yahoo.co.jp>
2021-06-30 10:34:33 +0900

    PF作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,3 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 end
 # , if: -> { Rails.env.production? && ENV['HEROKU_ENV'] != 'staging' }
-# ユーザーがログインしているかどうか確認する！before_action :authenticate_user!
